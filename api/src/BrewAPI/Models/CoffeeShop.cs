5bde068cae8b2b263f1726562aa32b1bb0086ff9
Jeff Ross <jross70@gatech.edu>
2016-10-24 23:11:18 -0400

    First commit- add the api project

---
+++ api/src/BrewAPI/Models/CoffeeShop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopCore.Models
+{
+    public class CoffeeShop
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+        public double MediumCoffeePrice { get; set; }
+    }
+}
